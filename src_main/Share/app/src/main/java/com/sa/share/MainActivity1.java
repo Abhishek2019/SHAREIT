40e67e5df9161b642a1df43ef948a150e6c4aa0f
Abhishek2019 <abhishekw13@gmail.com>
2018-01-26 17:26:25 +0530

    Abhishek2019:main page created

--- src_main/Share/app/src/main/java/com/sa/share/MainActivity1.java
+++ src_main/Share/app/src/main/java/com/sa/share/MainActivity1.java
@@ -1,7 +1,9 @@
 package com.sa.share;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity1 extends AppCompatActivity {
 
@@ -10,4 +12,20 @@ public class MainActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
     }
+
+    void onSender(View view){
+
+        Intent i = new Intent(this, MainActivity_sender.class);
+        startActivity(i);
+
+    }
+
+    void onReceiver(View view){
+        Intent i = new Intent(this, MainActivity_receiver.class);
+        startActivity(i);
+
+
+
+    }
+
 }
---
+++ src_main/Share/app/src/main/java/com/sa/share/MainActivity_sender.java
@@ -0,0 +1,13 @@
+package com.sa.share;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_sender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sender);
+    }
+}
