14b35f89e26503caa16ff818a204cac8a0c634cb
Abhishek2019 <abhishekw13@gmail.com>
2018-01-29 02:51:55 +0530

    Abhishek2019: Socket Connection Established

--- src_main/Share/app/src/main/java/com/sa/share/MainActivity_receiver.java
+++ src_main/Share/app/src/main/java/com/sa/share/MainActivity_receiver.java
@@ -1,9 +1,11 @@
 package com.sa.share;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -13,31 +15,54 @@ public class MainActivity_receiver extends AppCompatActivity {
     Socket clientSocket;
     String IP;
     int SERVERPORT = 2935;
+    boolean connected = false;
+    Handler handler;
 
+    TextView clientStatus;
     EditText serverIP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_receiver);
-
+        handler = new Handler();
         serverIP = (EditText)findViewById(R.id.edit_serverIP);
+        clientStatus = (TextView)findViewById(R.id.text_clientStatus);
 
     }
 
-    void connectClient(View view){
+    void connectServer(View view){
 
-        IP = serverIP.getText().toString();
+        //IP = serverIP.getText().toString();
 
-        try {
+            //clientSocket = new Socket(IP, SERVERPORT);
+        Thread clientThread = new Thread(new ClientThread());
+        clientThread.start();
+    }
 
-            clientSocket = new Socket(IP, SERVERPORT);
+    class ClientThread implements Runnable{
 
+        public void run(){
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            IP = serverIP.getText().toString();
+            try {
+                clientSocket = new Socket(IP, SERVERPORT);
+                connected = true;
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(connected) {
+                            clientStatus.setText("connected");
+                        }
+                    }
+                });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-
 
     }
 
--- src_main/Share/app/src/main/java/com/sa/share/MainActivity_sender.java
+++ src_main/Share/app/src/main/java/com/sa/share/MainActivity_sender.java
@@ -1,5 +1,6 @@
 package com.sa.share;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,35 +16,70 @@ public class MainActivity_sender extends AppCompatActivity {
     ServerSocket serverSocket;
     Socket sSocket;
     int SERVERPORT = 2935;
-
+    Handler handler;
     TextView listenText;
+    TextView serverStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sender);
+
         listenText = (TextView)findViewById(R.id.text_listen);
+        listenText.setText("Not Listening");
+        serverStatus = (TextView)findViewById(R.id.text_serverStatus);
+        serverStatus.setText("Disconnected");
+
+
+
+        handler = new Handler();
     }
 
     void startServer(View view){
 
-        try {
 
-            serverSocket = new ServerSocket();
+            Thread serverThread = new Thread(new ServerThread());
+            serverThread.start();
 
-            serverSocket.bind(new InetSocketAddress(SERVERPORT));
+    }
 
-            sSocket = serverSocket.accept();
-            listenText.setText("Listening....");
-
+    class ServerThread implements Runnable{
 
 
+        public void run(){
+            try {
 
-        } catch (IOException e) {
-            e.printStackTrace();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        listenText.setText("Listening on port : "+SERVERPORT);
+                    }
+                });
+
+                serverSocket = new ServerSocket(SERVERPORT);
+
+                while (true) {
+                    // LISTEN FOR INCOMING CLIENTS
+                    Socket client = serverSocket.accept();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            serverStatus.setText("Connected...");
+                        }
+                    });
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
         }
-
 
     }
 
 }
+
+
+
