6c6be9afe75e73c9922cc82d96d739d2b032475c
Abhishek2019 <abhishekw13@gmail.com>
2018-02-01 20:26:00 +0530

    QR code scanner implemented

--- src_main/Share/app/src/main/java/com/sa/share/MainActivity_receiver.java
+++ src_main/Share/app/src/main/java/com/sa/share/MainActivity_receiver.java
@@ -1,11 +1,24 @@
 package com.sa.share;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
@@ -21,11 +34,15 @@ public class MainActivity_receiver extends AppCompatActivity {
     boolean connected = false;
     boolean sending = false;
     Handler handler;
+    IntentIntegrator qrScan;
+
+    int PERMISSION_REQUEST_CODE = 1;
 
     TextView clientStatus;
     EditText serverIP;
 
     int filesize = 100000; // filesize temporary hardcoded
+
 
     long start = System.currentTimeMillis();
     int bytesRead;
@@ -36,15 +53,70 @@ public class MainActivity_receiver extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_receiver);
         handler = new Handler();
+
+        qrScan = new IntentIntegrator(this);
         serverIP = (EditText)findViewById(R.id.edit_serverIP);
         clientStatus = (TextView)findViewById(R.id.text_clientStatus);
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                Toast.makeText(this,"permission already granted",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+
+                    Toast.makeText(this,result.getContents(),Toast.LENGTH_LONG).show();
+                    //converting the data to json
+                    //JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    //textViewName.setText(obj.getString("name"));
+                    //textViewAddress.setText(obj.getString("address"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 
     void scanIP(View view){
 
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+        } else {
+            Toast.makeText(this,"permission already granted",Toast.LENGTH_LONG).show();
+            qrScan.initiateScan();
 
+        }
 
     }
 
